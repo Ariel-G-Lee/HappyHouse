bcf4583ef2428af9dfd30ac0901b6a33dd70c059
ritajeong <litch0s2@gmail.com>
2021-05-23 17:49:33 +0900

    update house search

--- src/main/java/com/ssafy/house/service/HouseService.java
+++ src/main/java/com/ssafy/house/service/HouseService.java
@@ -7,4 +7,5 @@ import com.ssafy.house.dto.HouseDto;
 public interface HouseService {
 	public List<HouseDto> searchByDong(String dongCode);
 	public List<HouseDto> searchByName(String aptName);
+	public List<HouseDto> searchAll();
 }
