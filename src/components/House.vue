e55005abd8f5631dbbb7d5be8853d538b88fa69f
ritajeong <litch0s2@gmail.com>
2021-05-23 13:38:28 +0900

    complete house search and map

--- src/components/House.vue
+++ src/components/House.vue
@@ -12,16 +12,17 @@
               <div class="form-group mt-3">
                 <div class="d-flex justify-content-center mt-2"> 
                   <div class="selectbox me-2">
-                    <select class="form-select py-2 btn btn-light">
-                      <option value="dong" selected="selected">동 </option>
-                      <option value="AptName">아파트명 </option> 
+                    <select v-model="searchOption" class="form-select py-2 btn btn-light" name="key" id="key">
+                      <option selected>동/아파트명</option>
+                      <option value="dong">동 </option>
+                      <option value="aptName">아파트명 </option> 
                     </select>
                   </div>
                   <div>
-                    <input id="searchKeyword" type="text" class="form-control me-2" placeholder="검색어 입력" name="word">
+                    <input v-model="searchKeyword" id="searchKeyword" type="text" class="form-control me-2" placeholder="검색어 입력" name="word">
                   </div> 
                   <div> 
-                    <button id="apartSearch" type="button" class="btn btn-green">검색</button>
+                    <button @click.prevent="apartSearch" class="btn btn-green">검색</button>
                   </div>
                 </div>
               </div>
@@ -49,9 +50,49 @@
 import HouseDetail from './HouseDetail.vue'
 import HouseList from './HouseList.vue'
 import HouseMap from './HouseMap.vue'
+import http from "@/common/axios.js";
 export default {
+  name:'House',
   components: { HouseList, HouseMap, HouseDetail},
-  
+  data(){
+    return{
+      searchOption:'동/아파트명',
+      searchKeyword:'',
+    }
+  }, 
+  created() {
+    this.searchOption = '동/아파트명'
+  },
+  methods:{
+    apartSearch(){
+      if(this.searchKeyword == '')
+        alert("전체 검색되게") 
+      console.log(this.searchOption);
+      console.log(this.searchKeyword);
+      this.dongSearch();
+    }, 
+    dongSearch(){
+      http.get(
+          '/houses',
+          {
+            params : {
+              key : this.searchOption,
+              word : this.searchKeyword
+            }
+          }
+          )
+          .then(({ data }) => { 
+            console.log("success");
+            console.log("현재 데이터 리스트 : ", data);
+            console.log("총 갯수 : ", Object.keys(data).length);	
+            this.list = data.list;
+          })
+          .catch((error) => {
+            console.log("search houses : error ");
+            console.log(error);
+          });
+    }
+  }
 }
 </script>
 <style>
