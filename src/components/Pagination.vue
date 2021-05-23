ef8fa1c4e20cef57e89e58ad0726ceaa128241cc
이가빈(Ariel) <kv829ly@naver.com>
2021-05-23 16:50:07 +0900

    notice detail, list, modify

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,18 +1,18 @@
 <template>
-  <nav aria-label="Page navigation">
+  <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
       <li v-if="prev" class="page-item">
-        <a class="page-link" href="#" aria-label="Previous" @click="paginationChanged(startPageIndex - 1)">
+        <a class="page-link text-dark" href="#" aria-label="Previous" @click="paginationChanged(startPageIndex - 1)">
           <span aria-hidden="true">«</span>
         </a>
       </li>
       <li v-for="index in ( endPageIndex-startPageIndex + 1 )" :key="index"
           v-bind:class="{active: (startPageIndex + index - 1 == $store.state.notice.currentPageIndex)}" class="page-item">
         <a @click="paginationChanged(startPageIndex + index - 1)" 
-           class="page-link" href="#">{{ startPageIndex + index - 1 }}</a> <!-- href 는 그대로, 커서 모양 유지-->
+           class="page-link text-dark" href="#">{{ startPageIndex + index - 1 }}</a> <!-- href 는 그대로, 커서 모양 유지-->
       </li>
       <li v-if="next" class="page-item">
-        <a class="page-link" href="#" aria-label="Next" @click="paginationChanged(endPageIndex + 1)">
+        <a class="page-link text-dark" href="#" aria-label="Next" @click="paginationChanged(endPageIndex + 1)">
           <span aria-hidden="true">»</span>
         </a>
       </li>
@@ -72,4 +72,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.page-item.active .page-link {
+  background-color: #34b7a7;
+  border-color: #34b7a7;
+}
+</style>
