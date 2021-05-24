2bc0338a064cc9c6c90e4a89e471a090aaaf5e81
ritajeong <litch0s2@gmail.com>
2021-05-24 21:46:09 +0900

    update LineChart.vue

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,14 +1,51 @@
 <script>
-import { Line } from 'vue-chartjs'
+import { Bar } from "vue-chartjs";
 
 export default {
-  extends: Line,
-  props: ['chartdata', 'options'],
-  mounted () {
-    this.renderChart(this.chartdata, this.options)
+  extends: Bar,
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ],
+        datasets: [
+          {
+            label: "동물병원",
+            backgroundColor:  "#dc3545",
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          },
+          {
+            label: "동물약국",
+            backgroundColor: "#0dcaf0",
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      },
+      { 
+        responsive: true, maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Bar Chart'
+          }
+        } 
+      }
+    );
   }
-}
+};
 </script>
-
-<style>
-</style>
