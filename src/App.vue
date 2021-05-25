4045a52efd94fd4a71a36fbb84ceb54cc44d3b9d
ritajeong <litch0s2@gmail.com>
2021-05-25 21:25:51 +0900

    delete apptop.vue

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,7 @@
     <!--v-bins:isLogin 으로 로그인 여부, userInfo로 로그인 정보를 navBar쪽에 props로 보내준다. -->
     <app-nav></app-nav>
     <router-view></router-view>
-    <app-footer></app-footer>
-    <app-top></app-top>
+    <app-footer></app-footer> 
     <!-- 로그인이 성공하면 emit으로 부모 호출한다 -> loginSuccess -->
   </div>
 </template>
@@ -12,16 +11,14 @@
 <script>
 
 import AppNav from './components/AppNav.vue'
-import AppFooter from './components/AppFooter.vue'
-import AppTop from './components/AppTop.vue'
+import AppFooter from './components/AppFooter.vue' 
 
 
 export default {
   name: 'App',
   components: {
     AppNav,
-    AppFooter,
-    AppTop,
+    AppFooter, 
   }
 }
 </script>
