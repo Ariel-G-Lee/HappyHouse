87b369dbdd27f094fb174df844bd0facf7868d05
ritajeong <litch0s2@gmail.com>
2021-05-26 10:23:45 +0900

    arrange animal name

--- src/components/AnimalName.vue
+++ src/components/AnimalName.vue
@@ -1,29 +1,28 @@
-
 <template>
-  <div id="app">
-    <cloud :data="words" :fontSizeMapper="fontSizeMapper" />
-  </div>
+	<div id="anlmai-name">
+		<cloud :data="words" :fontSizeMapper="fontSizeMapper" />
+	</div>
 </template>
 
 <script>
 import Cloud from 'vue-d3-cloud'
-
+//animalname get
 export default {
-    name: 'app',
-    data() {
-        return {
-            words: [
-                { text: 'Vue', value: 1000 },
-                { text: 'js', value: 200 },
-                { text: 'is', value: 800 },
-                { text: 'very cool', value: 1000000 },
-                { text: 'lunch', value: 100 },
-            ],
-            fontSizeMapper: word => Math.log2(word.value) * 5,
-        }
-    },
-    components: {
-        Cloud,
-    },
+	name: 'AnimalName',
+	data() {
+		return {
+			words: [
+				{ text: 'Vue', value: 1000 },
+				{ text: 'js', value: 200 },
+				{ text: 'is', value: 800 },
+				{ text: 'very cool', value: 1000000 },
+				{ text: 'lunch', value: 100 },
+			],
+			fontSizeMapper: word => Math.log2(word.value) * 5,
+		}
+	},
+	components: {
+		Cloud,
+	},
 }
 </script>
