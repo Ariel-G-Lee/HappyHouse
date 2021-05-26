5ff442cac4f8124fbfa47e438bce6697c0ef85f9
ritajeong <litch0s2@gmail.com>
2021-05-26 13:30:08 +0900

    update AnimalName.vue

--- src/components/AnimalName.vue
+++ src/components/AnimalName.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="anlmai-name">
-      <cloud>
-      </cloud>
+      <!-- <cloud></cloud> -->
   </div>
 </template>
 
 <script>
-import Cloud from 'vue-d3-cloud';
-//animalname get
-export default {
-	name: 'AnimalName',
-	data() {
-		return {
-			loaded: false,
-			words: [],
-			color: 'Category10',
-			fontSizeMapper: word => Math.log2(word.value*20)*3,
-			font: "Times New Roman",
-			colorCount: 2,
-		}
-	},
-	components: {
-		Cloud,
-	},
-	created(){
+// import Cloud from 'vue-d3-cloud';
+// //animalname get
+// export default {
+// 	name: 'AnimalName',
+// 	data() {
+// 		return {
+// 			loaded: false,
+// 			words: [],
+// 			color: 'Category10',
+// 			fontSizeMapper: word => Math.log2(word.value*20)*3,
+// 			font: "Times New Roman",
+// 			colorCount: 2,
+// 		}
+// 	},
+// 	components: {
+// 		Cloud,
+// 	},
+// 	created(){
 		
-	},
-}
+// 	},
+// }
 </script>
 
 <style>
